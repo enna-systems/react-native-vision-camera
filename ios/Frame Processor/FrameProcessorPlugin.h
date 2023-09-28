519a947f49e57eddbdfe235fba03582a10444a7b
Timo Köhler <t.koehler@enna.care>
2023-09-28 22:17:58 +0200

    format

--- ios/Frame Processor/FrameProcessorPlugin.h
+++ ios/Frame Processor/FrameProcessorPlugin.h
@@ -14,9 +14,8 @@
 #import "Frame.h"
 
 @protocol FrameProcessorPluginBase
-+ (id) callback:(Frame*)frame withArgs:(NSArray<id>*)args;
++ (id)callback:(Frame *)frame withArgs:(NSArray<id> *)args;
 @end
-
 
 #define VISION_CONCAT2(A, B) A##B
 #define VISION_CONCAT(A, B) VISION_CONCAT2(A, B)
@@ -31,33 +30,36 @@
  * The JS function will have the same name as the given Objective-C function, but with a "__" prefix.
  * Make sure to add that function to the babel.config.js under reanimated's "globals" option, and add TypeScript type declarations.
  */
-#define VISION_EXPORT_FRAME_PROCESSOR(frame_processor)                              \
-                                                                                    \
-+(void)load                                                                         \
-{                                                                                   \
-  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #frame_processor callback:^id(Frame* frame, NSArray<id>* args) { \
-    return frame_processor(frame, args);                                           \
-  }];                                                                               \
-}
-
+#define VISION_EXPORT_FRAME_PROCESSOR(frame_processor)                                           \
+                                                                                                 \
+  +(void)load                                                                                    \
+  {                                                                                              \
+    [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #frame_processor               \
+                                                 callback:^id(Frame *frame, NSArray<id> *args) { \
+                                                   return frame_processor(frame, args);          \
+                                                 }];                                             \
+  }
 
 /**
  * Same as VISION_EXPORT_FRAME_PROCESSOR, but uses __attribute__((constructor)) for
  * registration. Useful for registering swift classes that forbids use of +(void)load.
  */
-#define VISION_EXPORT_SWIFT_FRAME_PROCESSOR(name, objc_name) \
-objc_name : NSObject<FrameProcessorPluginBase>                                      \
-@end                                                                                \
-                                                                                    \
-@interface objc_name (FrameProcessorPlugin)                                         \
-@end                                                                                \
-@implementation objc_name (FrameProcessorPlugin)                                    \
-                                                                                    \
-+(void)load                                                                          \
-{                                                                                   \
-  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #name callback:^id(Frame* frame, NSArray<id>* args) {    \
-    return [objc_name callback:frame withArgs:args];                               \
-  }];                                                                               \
-}
+#define VISION_EXPORT_SWIFT_FRAME_PROCESSOR(name, objc_name)                                        \
+  objc_name:                                                                                        \
+  NSObject<FrameProcessorPluginBase> @                                                              \
+  end                                                                                               \
+                                                                                                    \
+      @interface                                                                                    \
+      objc_name(FrameProcessorPlugin)                                                               \
+  @end                                                                                              \
+  @implementation objc_name (FrameProcessorPlugin)                                                  \
+                                                                                                    \
+  +(void)load                                                                                       \
+  {                                                                                                 \
+    [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #name                             \
+                                                 callback:^id(Frame *frame, NSArray<id> *args) {    \
+                                                   return [objc_name callback:frame withArgs:args]; \
+                                                 }];                                                \
+  }
 
 #endif /* FrameProcessorPlugin_h */
